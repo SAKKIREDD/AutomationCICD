ae57e875f8fcf12b45caeaf5704df5816feb5dfb
sakkiredd <sivacse502@gmail.com>
2024-11-01 16:23:10 +0530

    hello monday

--- src/test/java/Day1.java
+++ src/test/java/Day1.java
@@ -6,7 +6,7 @@ public class Day1 {
     @Test(groups = {"smoke1"})
     @Parameters({"URL","Key"})
     public void monday(@Optional String URL,@Optional String key) {
-        System.out.println("hello monday. i am updating print statement");
+        System.out.println("hello monday.");
         System.out.println("url: "+URL);
         System.out.println("key: "+key);
            }
